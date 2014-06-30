d53fb19331837afb10aa1b6304518a805bab7f2c
Y. Kazancev <yegor@carbonsoft.ru>
2014-06-30 15:24:14 +0600

    before size

--- make_zip.sh
+++ make_zip.sh
@@ -3,7 +3,13 @@ set +ue
 set +x
 mkdir zip
 #zip -9 -r ./zip/${f}.zip ./map/$f
+touch ./before_zip
 for oblname in `cat to_names | awk -F ";" '{ print $1}'`.
 do
+  touch ./before_zip
+  
+  SIZE=`stat -c"%s" ./map/${oblname}.map`
+  echo ${oblname}:$SIZE >> ./before_zip
   zip -D -9 -r ./zip/${oblname}.zip    ./map/${oblname}.map
+  
 done
