d5a9fa4b35101b618c2a169a026a6e54b9d96d10
SB <admin@dotsb.net.ru>
2014-07-30 13:22:15 +0000

    Country to mysql add

--- russia_to_mysql.sh
+++ russia_to_mysql.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -x
 set +ue
-cat to_names | while read ip mac server; do
-    echo "INSERT INTO test (IP,MAC,SERVER) VALUES ('$ip', '$mac', '$server');"
-done |
+echo "TRUNCATE TABLE maps_files;" > to_names.sql
+cat to_names | while read line; do
+    #echo "INSERT INTO test (IP,MAC,SERVER) VALUES ('$ip', '$mac', '$server');"
+    #echo $line
+    file=`echo $line | awk -F ";" '{ print $1}'`
+    title=`echo $line | awk -F ";" '{ print $2}'`
+    bbox=`echo $line | awk -F ";" '{ print $3}'`
+    minlat=`echo $bbox | awk -F "," '{ print $1}'`
+    minlon=`echo $bbox | awk -F "," '{ print $2}'`
+    maxlat=`echo $bbox | awk -F "," '{ print $3}'`
+    maxlon=`echo $bbox | awk -F "," '{ print $4}'`
+    #echo $file
+    #echo $title
+    #echo $bbox
+    #echo $minlat
+    #echo $minlon
+    #echo $maxlat
+    #echo $maxlon
+    echo "INSERT INTO maps_files(mapid ,countryid ,location ,title ,file ,minlat ,minlon,maxlat,maxlon) VALUES (NULL ,  '1',  '${file}',  '${title}',  '${file}',  '${minlat}',  '${minlon}',  '${maxlat}',  '${maxlon}');" >> to_names.sql
+    #echo "
+done 
+mysql -utwmap -p123123 twmap <  to_names.sql
