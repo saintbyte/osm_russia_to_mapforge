3a17f13fd1451e3e2d8947802bb40047c150100e
Y. Kazancev <yegor@carbonsoft.ru>
2014-08-04 17:36:38 +0600

    Белоруссия 3

--- get_belarus.sh
+++ get_belarus.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # ISO Code 
 CODE=BY
+echo `date +%c` > ${CODE}
 set -x
 set -ue
 BBOX=""
-wget -P ./osmsrc http://download.geofabrik.de/europe/belarus-latest.osm.bz2
-bunzip2 -c ./osmsrc/belarus-latest.osm.bz2 > ./tmp/${CODE}.osm
-osmosis --rx file=./tmp/${CODE}.osm --mapfile-writer file=./map/BY.map type=hd  bbox=$BBOX
+cont_str=`cat int_names | grep "${CODE};"`
+BBOX=`echo ${cont_str} | awk -F ";" '{ print $3 }'`
+CENTER=`echo ${cont_str} | awk -F ";" '{ print $4 }'`
+ZOOM=`echo ${cont_str} | awk -F ";" '{ print $5 }'`
+
+#wget -P ./osmsrc http://download.geofabrik.de/europe/belarus-latest.osm.bz2
+#bunzip2 -c ./osmsrc/belarus-latest.osm.bz2 > ./tmp/${CODE}.osm
+#osmosis --rx file=./tmp/${CODE}.osm --mapfile-writer file=./map/BY.map type=hd  bbox=$BBOX map-start-position=${CENTER} map-start-zoom=${ZOOM}
+zip -D -9 -r ./zip/${CODE}.zip    ./map/${CODE}.map
+
+echo `date +%c` >> ${CODE}
